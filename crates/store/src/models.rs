65c12365b8312a77c20372fc1bbcad0c779b6c89
Awesxme <manasvi.d007@gmail.com>
2026-05-17 10:37:02 +0000

    added user model to store

---
+++ crates/store/src/models.rs
@@ -0,0 +1,12 @@
+use serde::Serialize;
+use sqlx::FromRow;
+
+#[derive(Debug, FromRow, Serialize)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    #[serde(skip_serializing)]
+    pub password_hash: String,
+    pub collateral_available: rust_decimal::Decimal,
+    pub collateral_locked: rust_decimal::Decimal,
+}
